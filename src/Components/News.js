9c869fd7923a5c8abbcf0bf097030e1a3d791f98
muhmdpp <connect.mhdpp@gmail.com>
2024-04-18 09:21:45 +0530

    17-4-24

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,17 +1,77 @@
 import React from 'react'
-import img from '../img/img.png'
+// import img from '../img/img.png'
 import news_thumb from '../img/img-news.png'
 function News() {
-  return (
+  return (  
     <>
         <div className="flex-col items-start main-container ml-52 mt-10">
-           <div className="title text-7xl" style={{color:"#d9dca7"}}>NewsLetter</div>
-           <div className="container-box w-9/12 h-52 bg-black pr-5 mt-4 opacity-35 rounded-3xl">
-                <div className="text-area"></div>
-                <div className="img-area">
-                    <img src={news_thumb} alt="" />
+           <div className="title text-7xl font-medium" style={{color:"#d9dca7"}}>NewsLetter</div>
+           <div className="flex container-box w-4/6 h-52  pr-5 mt-6  rounded-3xl " style={{backgroundColor:"#091e1a"}}> 
+           <div>
+                <div className="text-area text-white w-10/12 font-bold rounded-lg p-4 pl-8  text-3xl" style={{color:"#d9dca7"}}>Genomic Study Sheds Light on How Carnivorous<br/>Asian Pitcher Plants Acquired Signature Insect Trap</div>
+                
+                <div className="text-area text-white w-10/12 rounded-lg  pl-8 text-xl">nomic Stfef udy Sheds Light on Genomic Stfef udy Sheds Light on How Carnivorous Asian Pitcher Plants Acquired Signature Insect TrapGenomic Study Sheds Light on How Carnivorous Asian Pitcher Plants Acquired Signature Insect Trap</div>
                 </div>
+                <div className='bg-fixed'>
+                <div className=" img-sec h-40 w-40 items-center justify-around end-1 pt-4 pb-4 pr-3 object-cover bg-fixed">
+                    <img src={news_thumb} alt="" className='rounded-lg'/>
+                </div>
+                </div>
+               
            </div>
+
+           <div className="flex container-box w-4/6 h-52  pr-5 mt-6  rounded-3xl " style={{backgroundColor:"#091e1a"}}> 
+           <div>
+                <div className="text-area text-white w-10/12 font-bold rounded-lg p-4 pl-8  text-3xl" style={{color:"#d9dca7"}}>Genomic Study Sheds Light on How Carnivorous<br/>Asian Pitcher Plants Acquired Signature Insect Trap</div>
+                
+                <div className="text-area text-white w-10/12 rounded-lg  pl-8 text-xl">nomic Stfef udy Sheds Light on Genomic Stfef udy Sheds Light on How Carnivorous Asian Pitcher Plants Acquired Signature Insect TrapGenomic Study Sheds Light on How Carnivorous Asian Pitcher Plants Acquired Signature Insect Trap</div>
+                </div>
+                <div className='bg-fixed'>
+                <div className=" img-sec h-40 w-40 items-center justify-around end-1 pt-4 pb-4 pr-3 object-cover bg-fixed">
+                    <img src={news_thumb} alt="" className='rounded-lg'/>
+                </div>
+                </div>
+               
+           </div>
+
+
+           <div className="flex container-box w-4/6 h-52  pr-5 mt-6  rounded-3xl " style={{backgroundColor:"#091e1a"}}> 
+           <div>
+                <div className="text-area text-white w-10/12 font-bold rounded-lg p-4 pl-8  text-3xl" style={{color:"#d9dca7"}}>Genomic Study Sheds Light on How Carnivorous<br/>Asian Pitcher Plants Acquired Signature Insect Trap</div>
+                
+                <div className="text-area text-white w-10/12 rounded-lg  pl-8 text-xl">nomic Stfef udy Sheds Light on Genomic Stfef udy Sheds Light on How Carnivorous Asian Pitcher Plants Acquired Signature Insect TrapGenomic Study Sheds Light on How Carnivorous Asian Pitcher Plants Acquired Signature Insect Trap</div>
+                </div>
+                <div className='bg-fixed'>
+                <div className=" img-sec h-40 w-40 items-center justify-around end-1 pt-4 pb-4 pr-3 object-cover bg-fixed">
+                    <img src={news_thumb} alt="" className='rounded-lg'/>
+                </div>
+                </div>
+               
+           </div>
+
+
+           <div className="flex container-box w-4/6 h-52  pr-5 mt-6  rounded-3xl " style={{backgroundColor:"#091e1a"}}> 
+           <div>
+                <div className="text-area text-white w-10/12 font-bold rounded-lg p-4 pl-8  text-3xl" style={{color:"#d9dca7"}}>Genomic Study Sheds Light on How Carnivorous<br/>Asian Pitcher Plants Acquired Signature Insect Trap</div>
+                
+                <div className="text-area text-white w-10/12 rounded-lg  pl-8 text-xl">nomic Stfef udy Sheds Light on Genomic Stfef udy Sheds Light on How Carnivorous Asian Pitcher Plants Acquired Signature Insect TrapGenomic Study Sheds Light on How Carnivorous Asian Pitcher Plants Acquired Signature Insect Trap</div>
+                </div>
+                <div className='bg-fixed'>
+                <div className=" img-sec h-40 w-40 items-center justify-around end-1 pt-4 pb-4 pr-3 object-cover bg-fixed">
+                    <img src={news_thumb} alt="" className='rounded-lg'/>
+                </div>
+                </div>
+               
+           </div>
+
+           
+
+           
+
+
+           
+           
+           
         </div>
     </>
   )
