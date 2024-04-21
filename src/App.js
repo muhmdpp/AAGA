705795e5f3e4391d02e3281bdb1d5957a946b3bd
muhmdpp <connect.mhdpp@gmail.com>
2024-04-21 22:30:05 +0530

    table-of-contents

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import './App.css'
 import NavBar from './Components/NavBar'
 import News from './Components/News'
 import NewsInfo from './Components/NewsInfo'
-import {BrowserRouter as Router,Routes,Route,Navigate} from 'react-router-dom';
+import {BrowserRouter as Router,Routes,Route,Navigate, Link} from 'react-router-dom';
 
 function App() {
   return (
     <>
-    <div>
+    <h1 className='text-white text-center mt-32 font-bold'>Table of Contents</h1>
+    <div className='flex flex-col justify-center items-center text-white'>
       
+
+      <Link to='/splash'>Splash</Link>
+      <Link to='/navbar'>NavBar</Link>
+      <Link to='/news-page'>News </Link>
+      <Link to='news-info'>News Info</Link>
     </div>
     <Routes Routes>
       <Route path='/splash' element={<Splash/>} />
